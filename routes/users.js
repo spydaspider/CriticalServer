025d15450e6af5628d64d57cec58c7b20486b2dc
spydaspider <spydaspider@gmail.com>
2025-04-11 09:22:46 +0100

    Changed somethings

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { signup, login, verifyEmail,getAllUsers } = require('../controllers/user.js');
+const { signup, login, verifyEmail,getAllUsers, forgotPassword, verifyOTPAndResetPassword } = require('../controllers/user.js');
 router.post('/signup', signup);
 router.post('/login', login);
 router.get('/verifyEmail', verifyEmail);
 router.get('/allUsers', getAllUsers);
+router.post('/forgotPassword', forgotPassword);
+router.post('/resetPassword',verifyOTPAndResetPassword)
 module.exports = router;
